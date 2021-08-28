2ab52034106af746113345bab693d0827e6b741e
YaminThiriAung21 <40109409+YaminThiriAung21@users.noreply.github.com>
2021-08-28 22:16:08 +0630

    Add files via upload

---
+++ config/db.php
@@ -0,0 +1,6 @@
+<?php
+  // DB Params
+  define("DB_HOST", "db2.cnk9wdzreuey.ap-southeast-1.rds.amazonaws.com");
+  define("DB_USER", "root");
+  define("DB_PASS", "password");
+  define("DB_NAME", "MartVilla");
